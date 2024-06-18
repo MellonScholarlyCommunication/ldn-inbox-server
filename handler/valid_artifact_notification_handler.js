956b5c7f426930c3a6f1e314eff324908eb66335
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-06-18 10:04:59 +0200

    Adding checks for valid artifacts

---
+++ handler/valid_artifact_notification_handler.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const { ldPropertyAsId , isArtifact } = require('../lib/util.js');
+const logger = require('../lib/util.js').getLogger();
+
+/**
+ * Demonstration notification handler, that checks if the notification
+ * message contains an artifact that is known to the data node
+ */
+async function handle({path,options}) {
+    logger.info(`parsing notification ${path}`);
+    
+    try {
+        const json = JSON.parse(fs.readFileSync(path, { encoding: 'utf-8'}));
+        
+        let artifact = undefined;
+
+        if (ldPropertyAsId(json['context'])) {
+            artifact = ldPropertyAsId(json['context']);
+        }
+        else if (ldPropertyAsId(json['object'])) {
+            artifact = ldPropertyAsId(json['object']);
+        }
+        else {
+            logger.error(`failed to find valid context or object`);
+            return { path, options, success: false };
+        }
+
+        if (!artifact) {
+            logger.error(`failed to find artifact`);
+            return { path, options, success: false };
+        }
+
+        if (isArtifact(artifact,options)) {
+            return { path, options, success: true };
+        }
+        else {
+            logger.error(`artifact ${artifact} is not known here...`);
+            return { path, options, success: false };
+        }
+    }
+    catch(e) {
+        logger.error(`failed to process ${path}`);
+        logger.debug(e);
+        return { path, options, success: false };
+    }
+}
+
+module.exports = { handle };
